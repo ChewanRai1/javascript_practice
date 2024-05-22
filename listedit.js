2296e056f80b6900fa4a9066ee4eb82ab6fd8b6d
ChewanRai1 <220154@softwarica.edu.np>
2024-05-22 15:05:44 +0545

    list modification coding

---
+++ listedit.js
@@ -0,0 +1,39 @@
+// this push in list of JS
+
+// const t = [5, -2, 6];
+
+// t.push(8);
+
+// console.log(t.length);
+// console.log(t[2]);
+
+// t.forEach (value=>{
+//     console.log(value)
+// })
+// ------------------------------------------------------------
+// const t = [1,5,-2,7]
+// const t2 = t.concat(8)
+
+// console.log(t);
+// console.log(t2)
+
+//------------------------------------------------------------
+
+// const t = [2,3,1,4]
+
+// const m1 = t.map(value=>value*2)
+// console.log(m1)
+
+//------------------------------------------------------------
+
+// const t = [1,5,2]
+// const m1 = t.map(value=>'<li>'+value+'</li>')
+// console.log(m1)
+
+//------------------------------------------------------------
+
+// const t = [7,1,4,2]
+// const [first,second, ...rest]=t
+
+// console.log(first);
+// console.log(rest);
